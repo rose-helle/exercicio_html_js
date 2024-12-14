a86bace038b8c6b6f7b3223853f8a6ddd287f79f
Rose <rose.helle@live.com>
2024-12-13 23:59:44 -0300

    ajustes finais HTML, CSS E JS

--- main.js
+++ main.js
@@ -5,12 +5,16 @@ form.addEventListener('submit', function(e) {
 
     const numA = parseInt(document.getElementById('numberA').value);
     const numB = parseInt(document.getElementById('numberB').value);
+
+    document.querySelector('.positive').style.display = 'none';
+    document.querySelector('.equal').style.display = 'none';
+    document.querySelector('.negative').style.display = 'none';
     
-    if (numB > numA){
+        if (numB > numA){
         document.querySelector('.positive').style.display = 'block';
         } else if (numB === numA) {
             document.querySelector('.equal').style.display = 'block';
         } else {
             document.querySelector('.negative').style.display = 'block';
         }
-})
+    })
